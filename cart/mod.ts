4345cefbe583fe6ec81abc6ac8c22149aaaaca0b
Valentin Popov <kalentinvs@gmail.com>
2022-02-11 21:25:27 +0300

    Initial

---
+++ cart/mod.ts
@@ -0,0 +1,29 @@
+import Checkout from '../mod.ts';
+
+export default class YookassaCart {
+    items: Record<string, number>;
+    checkout: Checkout;
+    currency: string;
+    return_url: string;
+    constructor(checkout: Checkout, items: Record<string, number>, meta: Record<string, string>) {
+        this.items = items || []
+        this.checkout = checkout
+        this.currency = meta.currency
+        this.return_url = meta.return_url
+    }
+    async pay(description: string, items: string[]) {
+        const amount = items.map((item: string) => this.items[item]).reduce((a: number, b: number) => a + b, 0)
+        const payment = await this.checkout.execute({
+            amount: {
+                value: amount.toFixed(2),
+                currency: this.currency
+            },
+            confirmation: {
+                type: "redirect",
+                return_url: this.return_url
+            },
+            description
+        })
+        return payment.confirmation.confirmation_url;
+    }
+}
---
+++ mod.ts
@@ -0,0 +1,87 @@
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
+
+export interface Payment {
+    amount: {
+        value: string;
+        currency: string;
+    },
+    description?: string,
+    receipt?: {
+        customer: {
+            full_name?: string ;
+            inn?: string;
+            email?: string;
+            phone?: string;
+        },
+        items: {
+            description: string;
+            quantity: string;
+            amount: {
+                value: string;
+                currency: string;
+            },
+            vat_code?: number;
+            payment_subject?: string;
+            payment_mode?: string;
+            product_code?: string;
+            country_of_origin_code?: string;
+            customs_declaration_number?: string;
+            excise?: string;
+        }[],
+        tax_system_code?: number;
+        phone?: string;
+        email?: string;
+    };
+    recipient?: {
+        gateway_id: string;
+    };
+    payment_token?: string;
+    payment_method_id?: string;
+    payment_method_data?: {
+        type: string;
+        login?: string;
+    };
+    confirmation: {
+        type: "qr" | "embedded" | "external" | "mobile_application" | "redirect";
+        locale?: string;
+        return_url?: string;
+        enforce?: boolean;
+        confirmation_url?: string;
+    };
+    save_payment_method?: boolean;
+    capture?: boolean;
+    client_ip?: string;
+    metadata?: unknown;
+    merchant_customer_id?: string;
+}
+
+export default class Checkout {
+    url = "https://api.yookassa.ru/v3/payments";
+    request: RequestInit;
+    constructor(id: number, key: string) {
+        this.request = {
+            method: "POST",
+            headers: {
+                'Authorization': `Basic ` + btoa(`${id}:${key}`),
+                'Content-Type': 'application/json;charset=utf-8',
+                'Idempotence-Key': v4.generate()
+            }
+        }
+    }
+    execute(object: Payment): Promise<Payment> {
+        return new Promise((resolve, reject) => {
+            const _requestCopy = Object.assign({}, this.request)
+            _requestCopy.body = JSON.stringify(object)
+
+            fetch(this.url, _requestCopy).catch(reject).then(r => {
+                if (typeof r == 'undefined')
+                    reject("Invalid request object")
+                const decoder = new TextDecoderStream()
+                r?.body?.pipeThrough(decoder)
+                decoder.readable.getReader().read().then(string => {
+                    resolve(JSON.parse(string?.value || "{}"))
+                })
+            })
+        })
+    }
+}
